fd19fc02f30637f1244a427b907a1ec565d20ac7
Lindomar Silva <lindomar.silva@prec.ind>
2019-07-08 08:46:45 -0300

    Commit inicial

---
+++ src/Commands/Install.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace AgenciaMaior\Laravelramodnil\Commands;
+
+use Illuminate\Console\Command;
+use File;
+
+class Install extends Command
+{
+    const ROOT_PATH = __DIR__ . '/../../../../../';
+    const SOURCES_PATH = __DIR__ . '/../';
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'boiler:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        echo "Instalando ramodnil...\n";
+
+        echo "Instalando pacotes NPM...\n";
+
+        $this->installNpmPackages();
+
+        echo "\033[32mPacotes NPM instalados\033[0m\n";
+
+        $this->makeAuth();
+
+        echo "Copiando arquivos... ";
+
+        $this->copyMigrations();
+
+        $this->setModels();
+
+        $this->setControllers();
+
+        $this->setACL();
+
+        $this->setRoutes();
+
+        $this->setStyles();
+
+        $this->setScripts();
+
+        $this->setTemplates();
+
+        echo "\033[32mPronto\033[0m\n";
+
+        echo "\033[32mramodnil instalado com sucesso\033[0m\n";
+    }
+
+    public function installNpmPackages() {
+        $command = 'npm install';
+
+        exec($command);        
+
+        $npmLibraries = [
+            '@fortawesome/fontawesome-free',
+            'jquery-validation',
+            'datatables.net-bs4',
+            'select2',
+            '@ttskch/select2-bootstrap4-theme',
+            'jquery-maskmoney',
+            'jquery-mask-plugin',
+            'moment',
+            'numeral',
+            'webpack-jquery-ui',
+            'summernote',
+            'codemirror',
+            'chart.js',
+        ];
+
+        $command = 'npm i ' . implode(' ', $npmLibraries);
+
+        exec($command);
+    }
+
+    public function makeAuth() {
+        $command = 'php artisan make:auth --force';
+
+        echo "$command \n";
+
+        exec($command);
+    }
+
+    public function copyMigrations() {
+        File::copyDirectory(self::SOURCES_PATH . '/migrations', self::ROOT_PATH . '/database/migrations');
+    }
+
+    public function setACL() {
+        File::copyDirectory(self::SOURCES_PATH . '/Providers', self::ROOT_PATH . '/app/Providers');
+        File::copyDirectory(self::SOURCES_PATH . '/Policies', self::ROOT_PATH . '/app/Policies');
+    }
+
+    public function setStyles() {
+        File::copyDirectory(self::SOURCES_PATH . '/resources/sass', self::ROOT_PATH . '/resources/sass');
+    }
+
+    public function setScripts() {        
+        $command = 'php artisan vendor:publish --tag=bootstrap_forms_js --force';
+
+        exec($command);
+
+        File::copyDirectory(self::SOURCES_PATH . '/resources/js', self::ROOT_PATH . '/resources/js');
+
+        $bootstrapJsFile = self::ROOT_PATH . '/resources/js/bootstrap.js';
+        $requireCommand = "require('./ramodnil');";
+
+        $file = fopen($bootstrapJsFile, 'r');
+
+        $addLine = true;
+        while (($line = fgets($file)) !== false) {
+            if ($line == $requireCommand) {
+                $addLine = false;
+            }
+        }
+
+        fclose($file);
+
+        if ($addLine) {
+            $file = fopen($bootstrapJsFile, 'a');
+            fwrite($file, "\n\n" . $requireCommand);
+            fclose($file);
+        }
+    }
+
+    public function setModels() {
+        File::copyDirectory(self::SOURCES_PATH . '/models', self::ROOT_PATH . '/app');
+    }
+
+    public function setControllers() {
+        File::copyDirectory(self::SOURCES_PATH . '/Controllers', self::ROOT_PATH . '/app/Http/Controllers');
+    }
+
+    public function setTemplates() {
+        File::copyDirectory(self::SOURCES_PATH . '/resources/views', self::ROOT_PATH . '/resources/views');
+    }
+
+    public function setI18n() {
+        File::copyDirectory(self::SOURCES_PATH . '/resources/lang', self::ROOT_PATH . '/resources/lang');
+    }
+
+    public function copyImages() {
+        File::copyDirectory(self::SOURCES_PATH . '/public/images', self::ROOT_PATH . '/public/images');
+    }
+
+    public function setRoutes() {
+        $routesFile = self::ROOT_PATH . '/routes/web.php';
+        $command = '\BeautifulSea\LaravelRamodnil\LaravelRamodnilServiceProvider::routes();';
+
+        $file = fopen($routesFile, 'r');
+
+        $addLine = true;
+        while (($line = fgets($file)) !== false) {
+            if ($line == $command) {
+                $addLine = false;
+            }
+        }
+
+        fclose($file);
+
+        if ($addLine) {
+            $file = fopen($routesFile, 'a');
+            fwrite($file, "\n\n" . $command);
+            fclose($file);
+        }
+    }
+}
---
+++ src/LaravelBeautifulSeaServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace BeautifulSea\LaravelRamodnil;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+class LaravelRamodnilServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->commands([
+            \BeautifulSea\LaravelRamodnil\Commands\Install::class,
+        ]);
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    public static function routes() {
+        Route::group(['prefix' => '/users'], function () {
+            Route::post('/first-user', 'UsersController@storeFirstUser')->name('users.first-user');
+            Route::get('/profile', 'UsersController@profile')->name('users.profile');
+            Route::post('/profile', 'UsersController@updateProfile')->name('users.save-profile');
+            Route::post('/check-email', 'UsersController@checkEmail')->name('users.check-email');
+            Route::post('/check-profile-email', 'UsersController@checkProfileEmail')->name('users.check-profile-email');
+            Route::post('/check-profile-password', 'UsersController@checkProfilePassword')->name('users.check-profile-password');
+            Route::get('/block/{user}', 'UsersController@block')->name('users.block');
+            Route::get('/unblock/{user}', 'UsersController@unblock')->name('users.unblock');
+        });
+        Route::resource('/users', 'UsersController');
+    }
+}
